b4def8b90081ac50daa8a761766e75b8afa4a2bd
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-03 12:39:10 +0200

    Create 00300-add-machines.sh

---
+++ os/00300-add-machines.sh
@@ -0,0 +1,13 @@
+juju add-machine node02
+juju add-machine node03
+juju add-machine node04
+juju add-machine node05
+juju add-machine node06
+juju add-machine node07
+juju add-machine node08
+juju add-machine node09
+juju add-machine node10
+juju add-machine node11
+juju add-machine node12
+juju add-machine node13
+juju add-machine node14
