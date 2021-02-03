33da43970d7e0c816436bac821ba64b447f1ccba
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-03 12:59:28 +0200

    Create 01000-deploy-keystone.sh

---
+++ os/01000-deploy-keystone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+juju deploy --config config/keystone.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:keystone keystone
+juju deploy --config config/keystone.yaml cs:hacluster keystone-hacluster
+juju add-relation keystone:ha keystone-hacluster:ha
+#
+juju add-relation keystone:shared-db mysql:shared-db
