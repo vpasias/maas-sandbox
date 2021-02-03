ae2e981806f5a1bc426068101b57935bc5486e0a
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-03 12:18:02 +0200

    Update 00100-remove-juju-bootstrap-controller.sh

--- os/00100-remove-juju-bootstrap-controller.sh
+++ os/00100-remove-juju-bootstrap-controller.sh
@@ -1,5 +1,5 @@
 # normal
-juju destroy-controller -y --destroy-all-models --destroy-storage maas-controller
+juju destroy-controller -y --destroy-all-models --destroy-storage maas-cloud-controller
 
 # Force Kill
 # last resort if your juju bootsrap controller inaccessible and something to removal fails.
