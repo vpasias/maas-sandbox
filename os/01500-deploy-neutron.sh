a7dc3b915dcef64fa9dfbde645dff56236787004
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-03 13:24:06 +0200

    Create 01500-deploy-neutron.sh

---
+++ os/01500-deploy-neutron.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+juju deploy --config config/neutron.yaml -n 3 --to 0,1,2 cs:neutron-gateway neutron-gateway
+juju deploy --config config/neutron.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:neutron-api neutron-api
+juju deploy cs:neutron-openvswitch neutron-openvswitch
+#
+juju deploy --config config/neutron.yaml cs:hacluster neutron-hacluster
+juju add-relation neutron-api:ha neutron-hacluster:ha
+#
+juju add-relation neutron-gateway:quantum-network-service nova-cloud-controller:quantum-network-service
+juju add-relation neutron-gateway:amqp rabbitmq-server:amqp
+#
+juju add-relation neutron-api:shared-db mysql:shared-db
+juju add-relation neutron-api:identity-service keystone:identity-service
+juju add-relation neutron-api:amqp rabbitmq-server:amqp
+#
+juju add-relation neutron-api:neutron-plugin-api neutron-gateway:neutron-plugin-api
+juju add-relation neutron-api:neutron-plugin-api neutron-openvswitch:neutron-plugin-api
+juju add-relation neutron-api:neutron-api nova-cloud-controller:neutron-api
+#
+juju add-relation neutron-openvswitch:amqp rabbitmq-server:amqp
+juju add-relation neutron-openvswitch:neutron-plugin nova-compute:neutron-plugin
