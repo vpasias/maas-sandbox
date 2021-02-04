d0a6b62e26b2946e4f561494c1cee7f6ec7066c6
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-04 18:22:26 +0200

    Update 01450-deploy-placement.sh

--- os/01450-deploy-placement.sh
+++ os/01450-deploy-placement.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-juju deploy --to lxd:0 --config openstack-origin=cloud:focal-victoria cs:placement placement
+juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 --config openstack-origin=cloud:focal-victoria cs:placement placement
+juju deploy cs:hacluster placement-hacluster
+juju add-relation placement:ha placement-hacluster:ha
 #
 juju add-relation placement mysql
 juju add-relation placement keystone
