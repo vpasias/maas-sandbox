b2a70ead63fec03c66e9ca73ae3bbaf601d37907
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-04 11:11:30 +0200

    Update 00200-create-juju-bootstrap-controller.sh

--- os/00200-create-juju-bootstrap-controller.sh
+++ os/00200-create-juju-bootstrap-controller.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-juju bootstrap --to node01 maas-cloud maas-cloud-controller
+juju bootstrap --to node01.maas --config default-series=focal maas-cloud maas-cloud-controller
 sleep 30
 juju add-model --config default-series=focal openstack
