bfa61edfa0bb9d696483dbeae23a7019b3ce5544
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-04 14:43:09 +0200

    Update 01000-deploy-keystone.sh

--- os/01000-deploy-keystone.sh
+++ os/01000-deploy-keystone.sh
@@ -4,6 +4,6 @@ juju deploy --config config/keystone.yaml cs:hacluster keystone-hacluster
 juju add-relation keystone:ha keystone-hacluster:ha
 #
 juju add-relation keystone:shared-db mysql:shared-db
-#juju deploy --config config/keystone.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:keystone keystone
-#juju deploy --config config/keystone.yaml cs:hacluster keystone-hacluster
+#juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 --config vip=192.168.10.231 keystone
+#juju deploy --config cluster_count=3 hacluster keystone-hacluster
 #juju add-relation keystone-hacluster:ha keystone:ha
