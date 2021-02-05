52270f0f50eb629ec7789f4da2fb5fd68b567c2f
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 13:39:07 +0200

    Update 01450-deploy-placement.sh

--- os/01450-deploy-placement.sh
+++ os/01450-deploy-placement.sh
@@ -3,6 +3,11 @@ juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 --config openstack-origin=cloud:focal-vi
 juju deploy cs:hacluster placement-hacluster
 juju add-relation placement:ha placement-hacluster:ha
 #
-juju add-relation placement mysql
-juju add-relation placement keystone
-juju add-relation placement nova-cloud-controller
+juju deploy mysql-router placement-mysql-router
+juju add-relation placement-mysql-router:db-router mysql-innodb-cluster:db-router
+juju add-relation placement-mysql-router:shared-db placement:shared-db
+#
+juju add-relation placement:identity-service keystone:identity-service
+juju add-relation placement:placement nova-cloud-controller:placement
+#
+juju add-relation placement:certificates vault:certificates
