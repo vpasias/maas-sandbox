917de3d3c0ea34c8be785716b2477408512cd761
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 13:55:59 +0200

    Update 01600-deploy-openstack-dashboard.sh

--- os/01600-deploy-openstack-dashboard.sh
+++ os/01600-deploy-openstack-dashboard.sh
@@ -3,5 +3,10 @@ juju deploy --config config/openstack-dashboard.yaml -n 3 --to lxd:0,lxd:1,lxd:2
 juju deploy --config config/openstack-dashboard.yaml cs:hacluster openstack-dashboard-hacluster
 juju add-relation openstack-dashboard:ha openstack-dashboard-hacluster:ha
 #
-juju add-relation openstack-dashboard:shared-db mysql:shared-db
+juju deploy mysql-router dashboard-mysql-router
+juju add-relation dashboard-mysql-router:db-router mysql-innodb-cluster:db-router
+juju add-relation dashboard-mysql-router:shared-db openstack-dashboard:shared-db
+#
 juju add-relation openstack-dashboard:identity-service keystone:identity-service
+#
+juju add-relation openstack-dashboard:certificates vault:certificates
