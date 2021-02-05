52d42c4a32176b2f5a1d41300e12b9bf7bc2b89d
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 13:32:09 +0200

    Update 01300-deploy-nova-cloud-controller.sh

--- os/01300-deploy-nova-cloud-controller.sh
+++ os/01300-deploy-nova-cloud-controller.sh
@@ -3,7 +3,10 @@ juju deploy --config config/nova-cloud-controller.yaml -n 3 --to lxd:0,lxd:1,lxd
 juju deploy --config config/nova-cloud-controller.yaml cs:hacluster ncc-hacluster
 juju add-relation nova-cloud-controller:ha ncc-hacluster:ha
 #
-juju add-relation nova-cloud-controller:shared-db mysql:shared-db
+juju deploy mysql-router ncc-mysql-router
+juju add-relation ncc-mysql-router:db-router mysql-innodb-cluster:db-router
+juju add-relation ncc-mysql-router:shared-db nova-cloud-controller:shared-db
+#
 juju add-relation nova-cloud-controller:identity-service keystone:identity-service
 juju add-relation nova-cloud-controller:amqp rabbitmq-server:amqp
 #
