a0c175b487ca309e7d974d08c4beb5ca2d4d9da0
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 12:54:02 +0200

    Update 00600-deploy-vault.sh

--- os/00600-deploy-vault.sh
+++ os/00600-deploy-vault.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+juju deploy --config config/vault.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:percona-cluster mysql
+juju deploy --config config/vault.yaml cs:hacluster mysql-hacluster
+juju add-relation mysql:ha mysql-hacluster:ha
+juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 --config vip=192.168.10.240 cs:vault vault
+juju deploy --config cluster_count=3 hacluster vault-hacluster
+juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 etcd
+juju deploy --to lxd:0 cs:~containers/easyrsa
+juju add-relation vault:ha vault-hacluster:ha
+juju add-relation vault:shared-db percona-cluster:shared-db
+juju add-relation etcd:db vault:etcd
+juju add-relation etcd:certificates easyrsa:client
