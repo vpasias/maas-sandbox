07c9e449f40663d2724330b27203edbcd9a66b83
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 13:25:06 +0200

    Update 01100-deploy-glance.sh

--- os/01100-deploy-glance.sh
+++ os/01100-deploy-glance.sh
@@ -3,7 +3,10 @@ juju deploy --config config/glance.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:glance gl
 juju deploy --config config/glance.yaml cs:hacluster glance-hacluster
 juju add-relation glance:ha glance-hacluster:ha
 #
-juju add-relation glance:shared-db mysql:shared-db
+juju deploy mysql-router glance-mysql-router
+juju add-relation glance-mysql-router:db-router mysql-innodb-cluster:db-router
+juju add-relation glance-mysql-router:shared-db glance:shared-db
+#
 juju add-relation glance:identity-service keystone:identity-service
 juju add-relation glance:amqp rabbitmq-server:amqp
 #
