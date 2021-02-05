ff18716f1285523826670b29dd7f2917f241f42f
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 13:07:30 +0200

    Update 00600-deploy-vault.sh

--- os/00600-deploy-vault.sh
+++ os/00600-deploy-vault.sh
@@ -3,7 +3,7 @@ juju deploy --config config/vault.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:percona-cl
 juju deploy --config config/vault.yaml cs:hacluster mysql-hacluster
 juju add-relation mysql:ha mysql-hacluster:ha
 juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 --config vip=192.168.10.240 cs:vault vault
-juju deploy --config cluster_count=3 hacluster vault-hacluster
+juju deploy --config config/vault.yaml cs:hacluster vault-hacluster
 juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 etcd
 juju deploy --to lxd:0 cs:~containers/easyrsa
 juju add-relation vault:ha vault-hacluster:ha
