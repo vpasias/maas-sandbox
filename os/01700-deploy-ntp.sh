4cf9dadf1e3075116e691c71389c1f662c1aa243
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 13:59:09 +0200

    Update 01700-deploy-ntp.sh

--- os/01700-deploy-ntp.sh
+++ os/01700-deploy-ntp.sh
@@ -3,4 +3,3 @@ juju deploy --config config/ntp.yaml cs:ntp ntp
 juju add-relation ntp:juju-info ceph-osd:juju-info
 juju add-relation ntp:juju-info ceph-osd-backup:juju-info
 juju add-relation ntp:juju-info nova-compute:juju-info
-juju add-relation ntp:juju-info neutron-gateway:juju-info
