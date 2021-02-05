a2d4af9ae13320b58b7f9b81dc02cc4a4fc6208e
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 13:33:02 +0200

    Update 01050-deploy-ceph-radosgw.sh

--- os/01050-deploy-ceph-radosgw.sh
+++ os/01050-deploy-ceph-radosgw.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 juju deploy --config config/ceph-radosgw.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:ceph-radosgw ceph-radosgw
 juju deploy --config config/ceph-radosgw.yaml cs:hacluster ceph-radosgw-hacluster
+#
 juju add-relation ceph-radosgw:mon ceph-mon:radosgw
 juju add-relation ceph-radosgw:ha ceph-radosgw-hacluster:ha
 juju add-relation ceph-radosgw:identity-service keystone:identity-service
