7667eaf4fc904b0ec65eeaf8337dd886655ba7c7
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 13:11:16 +0200

    Update 00900-deploy-mysql.sh

--- os/00900-deploy-mysql.sh
+++ os/00900-deploy-mysql.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 juju deploy --config config/mysql.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:mysql-innodb-cluster mysql-innodb-cluster
-juju deploy mysql-router keystone-mysql-router
-juju add-relation keystone-mysql-router:db-router mysql-innodb-cluster:db-router
-juju add-relation keystone-mysql-router:shared-db keystone:shared-db
