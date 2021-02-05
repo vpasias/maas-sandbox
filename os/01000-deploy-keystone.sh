1216636d853b459b64033ed78029de56417a08ff
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-05 13:20:49 +0200

    Update 01000-deploy-keystone.sh

--- os/01000-deploy-keystone.sh
+++ os/01000-deploy-keystone.sh
@@ -3,8 +3,8 @@ juju deploy --config config/keystone.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:keyston
 juju deploy --config config/keystone.yaml cs:hacluster keystone-hacluster
 juju add-relation keystone:ha keystone-hacluster:ha
 #
-juju add-relation keystone:shared-db mysql:shared-db
-juju add-relation keystone:identity-service cinder:identity-service
+juju deploy cs:mysql-router keystone-mysql-router
+juju add-relation keystone-mysql-router:db-router mysql-innodb-cluster:db-router
+juju add-relation keystone-mysql-router:shared-db keystone:shared-db
 #
-juju add-relation keystone:identity-service neutron-api:identity-service
 juju add-relation keystone:certificates vault:certificates
