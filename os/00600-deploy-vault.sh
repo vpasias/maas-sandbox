40a12bae94a5abeee53363db15f2626fb9f9a2b1
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-06 12:31:59 +0200

    Update 00600-deploy-vault.sh

--- os/00600-deploy-vault.sh
+++ os/00600-deploy-vault.sh
@@ -7,7 +7,7 @@ juju deploy --config config/vault.yaml cs:hacluster vault-hacluster
 juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 etcd
 juju deploy --to lxd:0 cs:~containers/easyrsa
 juju add-relation vault:ha vault-hacluster:ha
-juju add-relation vault:shared-db percona-cluster:shared-db
+juju add-relation vault:shared-db mysql:shared-db
 juju add-relation etcd:db vault:etcd
 juju add-relation etcd:certificates easyrsa:client
 # https://docs.openstack.org/project-deploy-guide/charm-deployment-guide/latest/app-ha.html
