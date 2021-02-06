395145c74c5f2e549a752961727b1bf8504d94ee
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-06 17:45:21 +0200

    Update 01250-deploy-cinder-backup.sh

--- os/01250-deploy-cinder-backup.sh
+++ os/01250-deploy-cinder-backup.sh
@@ -4,4 +4,4 @@ juju deploy cs:~openstack-charmers/cinder-backup cinder-backup
 juju add-relation cinder-backup:backup-backend cinder:backup-backend
 juju add-relation cinder-backup:ceph ceph-mon-backup:client
 #
-juju add-relation cinder-backup:certificates vault:certificates
+#juju add-relation cinder-backup:certificates vault:certificates
