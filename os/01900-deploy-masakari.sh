2db00a2f5e79d4a90c043ec7d1100a161e5124e5
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-07 11:23:11 +0200

    Update 01900-deploy-masakari.sh

--- os/01900-deploy-masakari.sh
+++ os/01900-deploy-masakari.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 juju deploy --config config/masakari.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:masakari masakari
 juju deploy --config config/masakari.yaml cs:hacluster masakari-hacluster
-juju deploy cs:masakari-monitors masakari-monitors 
+juju deploy cs:masakari-monitors masakari-monitors
+juju add-relation masakari:ha masakari-hacluster:ha
 #
 juju deploy mysql-router masakari-mysql-router
 juju add-relation masakari-mysql-router:db-router mysql-innodb-cluster:db-router
-juju add-relation masakari-mysql-router:shared-db cinder:shared-db
-!
+juju add-relation masakari-mysql-router:shared-db masakari:shared-db
+#
 juju add-relation masakari:identity-service keystone:identity-service
 juju add-relation masakari:amqp rabbitmq-server:amqp
+#
+juju add-relation cinder:ha cinder-hacluster:ha
+
 
