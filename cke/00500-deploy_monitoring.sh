912cc229e30d32dc331013db34afb46b3e7e93f5
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-08 15:47:52 +0200

    Update 00500-deploy_monitoring.sh

--- cke/00500-deploy_monitoring.sh
+++ cke/00500-deploy_monitoring.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-
+juju deploy nagios -n 1 --to lxd:13 --series=bionic cs:nagios nagios
+juju deploy nrpe --series=bionic
+juju expose nagios
+#
+juju add-relation nagios nrpe
+#
+juju add-relation nrpe kubernetes-master
+juju add-relation nrpe kubernetes-worker
+juju add-relation nrpe etcd
+juju add-relation nrpe easyrsa
+juju add-relation nrpe kubeapi-load-balancer
+#
+juju status --format yaml nagios/0 | grep public-address
+#
+juju ssh nagios/0 sudo cat /var/lib/juju/nagios.passwd
