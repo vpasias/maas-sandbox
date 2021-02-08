ae3dfdd372a41ee6bace77583d465d391086858c
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-08 14:48:34 +0200

    Create 00200-deploy_machines.sh

---
+++ cke/00200-deploy_machines.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+juju add-machine node02.maas
+juju add-machine node03.maas
+juju add-machine node04.maas
+juju add-machine node05.maas
+juju add-machine node06.maas
+juju add-machine node07.maas
+juju add-machine node08.maas
+juju add-machine node09.maas
+juju add-machine node10.maas
+juju add-machine node11.maas
+juju add-machine node12.maas
+juju add-machine node13.maas
+juju add-machine node14.maas
