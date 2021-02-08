6a72e40efacc459cf59661be9cd8893e927b332b
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-08 16:37:04 +0200

    Update 00500-deploy_logging.sh

--- cke/00500-deploy_logging.sh
+++ cke/00500-deploy_logging.sh
@@ -1,3 +1,3 @@
-
+#!/bin/bash
 wget https://raw.githubusercontent.com/charmed-kubernetes/kubernetes-docs/master/assets/graylog-vhost.tmpl
 juju config apache2 vhost_http_template="$(base64 ~/home/vagrant/maas-sandbox/cke/graylog-vhost.tmpl)"
