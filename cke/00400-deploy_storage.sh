c0a81b662104bcab4eca34f60758c1f9dd31d409
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-08 16:11:38 +0200

    Update 00400-deploy_storage.sh

--- cke/00400-deploy_storage.sh
+++ cke/00400-deploy_storage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 juju deploy -n 3 ceph-osd --storage osd-devices=32G,2 --storage osd-journals=8G,1
 #
-juju deploy -n 3 --to lxd:4,lxd:5,lxd:6 cs:ceph-mon ceph-mon
+juju deploy -n 3 --to lxd:7,lxd:8,lxd:9 cs:ceph-mon ceph-mon
 juju add-relation ceph-osd ceph-mon
 #
-juju deploy -n 3 --to lxd:4,lxd:6,lxd:6 cs:ceph-fs ceph-fs
+juju deploy -n 3 --to lxd:7,lxd:8,lxd:9 cs:ceph-fs ceph-fs
 juju add-relation ceph-fs ceph-mon
 #
 juju add-relation ceph-mon:admin kubernetes-master
