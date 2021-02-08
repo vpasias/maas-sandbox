0350ff83c8aaa93f211b15dcd7f10c01b63c6f79
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-08 16:24:22 +0200

    Update 00350-deploy_hacluster.sh

--- cke/00350-deploy_hacluster.sh
+++ cke/00350-deploy_hacluster.sh
@@ -3,4 +3,17 @@ juju deploy hacluster
 juju config kubeapi-load-balancer ha-cluster-vip="192.168.10.231 192.168.10.232"
 juju relate kubeapi-load-balancer hacluster
 #
+mkdir ~/.kube
 juju scp kubernetes-master/0:config ~/.kube/config
+#
+kubectl cluster-info
+#
+kubectl get nodes
+#
+kubectl get pods
+#
+kubectl get services
+#
+juju run-action --wait grafana/0 get-login-info
+#
+juju config kubernetes-master enable-metrics=true
