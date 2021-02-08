207e14b9f6501821e3ca1821d8457de362bfdeed
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-08 16:39:54 +0200

    Update 00500-deploy_logging.sh

--- cke/00500-deploy_logging.sh
+++ cke/00500-deploy_logging.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/charmed-kubernetes/kubernetes-docs/master/assets/graylog-vhost.tmpl
 juju config apache2 vhost_http_template="$(base64 ~/home/vagrant/maas-sandbox/cke/graylog-vhost.tmpl)"
+#
+juju status --format yaml apache2/0 | grep public-address
+# public-address: <apache2-ip>
+juju run-action --wait graylog/0 show-admin-password
+# admin-password: <graylog-password>
