9f0c5586f791192d245e051341c778d2e20cd94c
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-08 16:10:45 +0200

    Update 00300-deploy_basic_cke.sh

--- cke/00300-deploy_basic_cke.sh
+++ cke/00300-deploy_basic_cke.sh
@@ -2,15 +2,17 @@
 wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/calico-overlay.yaml
 wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/monitoring-pgt-overlay.yaml
 #
-juju deploy charmed-kubernetes --overlay /home/vagrant/calico-overlay.yaml --overlay /home/vagrant/monitoring-pgt-overlay.yaml
+juju deploy charmed-kubernetes --overlay /home/vagrant/maas-sandbox/cke/calico-overlay.yaml --overlay /home/vagrant/maas-sandbox/cke/monitoring-pgt-overlay.yaml
 #
 sudo snap install kubectl --classic
+sudo snap install helm --classic  
 #
 juju add-unit -n 2 kubeapi-load-balancer
 juju deploy hacluster
 juju config kubeapi-load-balancer ha-cluster-vip="192.168.10.231 192.168.10.232"
 juju relate kubeapi-load-balancer hacluster
-#
+#   
+mkdir ~/.kube
 juju scp kubernetes-master/0:config ~/.kube/config
 #
 kubectl cluster-info
