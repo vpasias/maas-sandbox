3aec1c71d4e3242d981c990888c614805f12bfda
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-09 16:15:25 +0200

    Update 00700-deploy_metallb.sh

--- cke/00700-deploy_metallb.sh
+++ cke/00700-deploy_metallb.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+juju add-k8s k8s-cloud --controller $(juju switch | cut -d: -f1)
+#
+juju add-model metallb-system k8s-cloud
+#
+juju deploy cs:~containers/metallb
+#
+juju config metallb-controller iprange="192.168.1.240-192.168.1.241"
