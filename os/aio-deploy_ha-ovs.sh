8a4f0fdac2397dbd4427cccbfd9d00283d9ce855
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-10 11:20:51 +0200

    Update aio-deploy_ha-ovs.sh

--- os/aio-deploy_ha-ovs.sh
+++ os/aio-deploy_ha-ovs.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
-# create juju bootstrap controller
-juju bootstrap --to node01.maas --config default-series=focal maas-cloud maas-cloud-controller
-sleep 20
-juju add-model --config default-series=focal openstack
-sleep 10
-# add machines
-juju add-machine node02.maas
-juju add-machine node03.maas
-juju add-machine node04.maas
-juju add-machine node05.maas
-juju add-machine node06.maas
-juju add-machine node07.maas
-juju add-machine node08.maas
-juju add-machine node09.maas
-juju add-machine node10.maas
-juju add-machine node11.maas
-juju add-machine node12.maas
-juju add-machine node13.maas
-juju add-machine node14.maas
-sleep 600
-juju machines
+# Execute first:
+#
 #deploy ceph
 juju deploy --config config/ceph-osd.yaml -n 3 --to 6,7,8 cs:ceph-osd ceph-osd
 sleep 10
@@ -38,3 +19,4 @@ juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 cs:memcached memcached
 juju deploy --config config/pcmysql.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:percona-cluster mysql
 juju deploy --config config/pcmysql.yaml cs:hacluster mysql-hacluster
 juju add-relation mysql:ha mysql-hacluster:ha
+sleep 10
