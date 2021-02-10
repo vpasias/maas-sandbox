1fb16f33985204ca2c601d4255b13cb47cd627f0
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-10 11:36:10 +0200

    Update aio-deploy_ha-ovs.sh

--- os/aio-deploy_ha-ovs.sh
+++ os/aio-deploy_ha-ovs.sh
@@ -84,4 +84,18 @@ juju add-relation cinder-backup:backup-backend cinder:backup-backend
 juju add-relation cinder-backup:ceph ceph-mon-backup:client
 #
 sleep 10
+# deploy nova cloud controller
+juju deploy --config config/nova-cloud-controller.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:nova-cloud-controller nova-cloud-controller
+juju deploy --config config/nova-cloud-controller.yaml cs:hacluster ncc-hacluster
+juju add-relation nova-cloud-controller:ha ncc-hacluster:ha
+#
+juju add-relation nova-cloud-controller:shared-db mysql:shared-db
+juju add-relation nova-cloud-controller:identity-service keystone:identity-service
+juju add-relation nova-cloud-controller:amqp rabbitmq-server:amqp
+#
+juju add-relation nova-cloud-controller:memcache memcached:cache
+juju add-relation nova-cloud-controller:image-service glance:image-service
+juju add-relation nova-cloud-controller:cinder-volume-service cinder:cinder-volume-service
+#
+sleep 10
 #
