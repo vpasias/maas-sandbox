131ef7b65af9555a14e4bba943042d12e1fc529b
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-10 11:47:33 +0200

    Update aio-deploy_ha-ovs.sh

--- os/aio-deploy_ha-ovs.sh
+++ os/aio-deploy_ha-ovs.sh
@@ -150,4 +150,12 @@ juju add-relation openstack-dashboard:shared-db mysql:shared-db
 juju add-relation openstack-dashboard:identity-service keystone:identity-service
 #
 sleep 10
+# deploy ntp
+juju deploy --config config/ntp.yaml cs:ntp ntp
+juju add-relation ntp:juju-info ceph-osd:juju-info
+juju add-relation ntp:juju-info ceph-osd-backup:juju-info
+juju add-relation ntp:juju-info nova-compute:juju-info
+juju add-relation ntp:juju-info neutron-gateway:juju-info
+#
+sleep 10
 #
