edd161ff7de960be4f4c5d94386b7e83d772b0ee
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-10 11:37:55 +0200

    Update aio-deploy_ha-ovs.sh

--- os/aio-deploy_ha-ovs.sh
+++ os/aio-deploy_ha-ovs.sh
@@ -98,4 +98,14 @@ juju add-relation nova-cloud-controller:image-service glance:image-service
 juju add-relation nova-cloud-controller:cinder-volume-service cinder:cinder-volume-service
 #
 sleep 10
+# deploy nova compute
+juju deploy --config config/nova-compute.yaml -n 3 --to 3,4,5 cs:nova-compute nova-compute
+#
+juju add-relation nova-compute:cloud-compute nova-cloud-controller:cloud-compute
+juju add-relation nova-compute:amqp rabbitmq-server:amqp
+juju add-relation nova-compute:image-service glance:image-service
+juju add-relation nova-compute:ceph ceph-mon:client
+juju add-relation nova-compute:ceph-access cinder-ceph:ceph-access
+#
+sleep 10
 #
