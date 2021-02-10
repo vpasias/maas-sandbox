94d2cfb55d82676988a0fbb63e338c0f13178b16
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-10 11:30:09 +0200

    Update aio-deploy_ha-ovs.sh

--- os/aio-deploy_ha-ovs.sh
+++ os/aio-deploy_ha-ovs.sh
@@ -44,5 +44,16 @@ juju add-relation ceph-radosgw:ha ceph-radosgw-hacluster:ha
 juju add-relation ceph-radosgw:identity-service keystone:identity-service
 #
 sleep 10
+# deploy-glance
+juju deploy --config config/glance.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:glance glance
+juju deploy --config config/glance.yaml cs:hacluster glance-hacluster
+juju add-relation glance:ha glance-hacluster:ha
 #
-
+juju add-relation glance:shared-db mysql:shared-db
+juju add-relation glance:identity-service keystone:identity-service
+juju add-relation glance:amqp rabbitmq-server:amqp
+#
+juju add-relation glance:ceph ceph-mon:client
+#
+sleep 10
+#
