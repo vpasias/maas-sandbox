99f2e0ff269ec91a4e6d052e012b48d733903f4f
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-14 09:46:16 +0200

    Update aio-deploy_ha-ovs.sh

--- os/aio-deploy_ha-ovs.sh
+++ os/aio-deploy_ha-ovs.sh
@@ -159,39 +159,6 @@ juju add-relation ntp:juju-info nova-compute:juju-info
 juju add-relation ntp:juju-info neutron-gateway:juju-info
 #
 sleep 10
-# deploy nagios
-juju deploy --config config/nagios.yaml -n 1 --to lxd:12 cs:nagios nagios
-juju deploy --config config/nagios.yaml cs:ntp nagios-ntp
-juju deploy cs:nrpe nrpe
-juju add-relation nagios:juju-info nagios-ntp:juju-info
-juju add-relation nagios:monitors nrpe:monitors
-juju add-relation nrpe:nrpe-external-master ceph-mon:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master ceph-mon-backup:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master ceph-osd:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master ceph-osd-backup:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master ceph-radosgw:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master ceph-radosgw-hacluster:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master cinder:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master cinder-hacluster:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master glance:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master glance-hacluster:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master keystone:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master keystone-hacluster:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master memcached:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master mysql:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master mysql-hacluster:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master ncc-hacluster:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master neutron-api:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master neutron-gateway:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master neutron-hacluster:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master nova-cloud-controller:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master nova-compute:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master ntp:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master openstack-dashboard:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master openstack-dashboard-hacluster:nrpe-external-master
-juju add-relation nrpe:nrpe-external-master rabbitmq-server:nrpe-external-master
-#
-sleep 10
 # deploy masakari
 juju deploy --config config/masakari.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:masakari masakari
 juju deploy --config config/masakari.yaml cs:hacluster masakari-hacluster
