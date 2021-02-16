9756e8cbabbce5fd3f8c50d34b1a6ec826d0fa2f
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-16 17:59:08 +0200

    Update 00350-deploy_hacluster.sh

--- cke/00350-deploy_hacluster.sh
+++ cke/00350-deploy_hacluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-juju add-unit -n 2 kubeapi-load-balancer
+juju add-unit -n 2 --to 8,9 kubeapi-load-balancer
 juju deploy hacluster
 juju config kubeapi-load-balancer ha-cluster-vip="192.168.10.231 192.168.10.232"
 juju relate kubeapi-load-balancer hacluster
