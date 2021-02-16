e19b8f567c490a005168fba6ea587afae7ed64f8
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-16 18:16:57 +0200

    Update 00300-deploy_basic_cke.sh

--- cke/00300-deploy_basic_cke.sh
+++ cke/00300-deploy_basic_cke.sh
@@ -3,9 +3,8 @@ wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays
 #wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/monitoring-pgt-overlay.yaml
 #wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/logging-egf-overlay.yaml
 #
-juju deploy ./bundle.yaml --overlay /home/vagrant/maas-sandbox/cke/calico-overlay.yaml --overlay monitoring-overlay.yaml
-#
-# juju deploy charmed-kubernetes --overlay /home/vagrant/maas-sandbox/cke/calico-overlay.yaml --overlay /home/vagrant/maas-sandbox/cke/monitoring-pgt-overlay.yaml --overlay /home/vagrant/maas-sandbox/cke/logging-egf-overlay.yaml
+juju deploy bundle.yaml --overlay calico-overlay.yaml --overlay monitoring-overlay.yaml --overlay logging-overlay.yaml
+# juju deploy bundle.yaml --overlay calico-overlay.yaml --overlay monitoring-overlay.yaml 
 #
 sudo snap install kubectl --classic
 sudo snap install helm --classic
