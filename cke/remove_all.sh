c112cee57b4a10182f9b6001111de57e139c2be8
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-16 22:10:45 +0200

    Update remove_all.sh

--- cke/remove_all.sh
+++ cke/remove_all.sh
@@ -4,4 +4,4 @@ juju destroy-controller -y --destroy-all-models --destroy-storage maas-cloud-con
 
 # Force Kill
 # last resort if your juju bootsrap controller inaccessible and something to removal fails.
-juju kill-controller -y maas-cloud-controller
+juju kill-controller -y maas-cloud-controller -t 5s
