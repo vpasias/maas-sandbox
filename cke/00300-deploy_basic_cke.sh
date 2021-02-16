881be07c1587a2e228f5944c376405c96a5cbdcb
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-16 17:51:47 +0200

    Update 00300-deploy_basic_cke.sh

--- cke/00300-deploy_basic_cke.sh
+++ cke/00300-deploy_basic_cke.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/calico-overlay.yaml
-wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/monitoring-pgt-overlay.yaml
+#wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/monitoring-pgt-overlay.yaml
 wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/logging-egf-overlay.yaml
 #
-juju deploy ./bundle.yaml --overlay /home/vagrant/maas-sandbox/cke/calico-overlay.yaml --overlay /home/vagrant/maas-sandbox/cke/monitoring-pgt-overlay.yaml
+juju deploy ./bundle.yaml --overlay /home/vagrant/maas-sandbox/cke/calico-overlay.yaml --overlay monitoring-pgt-overlay.yaml
 #
 # juju deploy charmed-kubernetes --overlay /home/vagrant/maas-sandbox/cke/calico-overlay.yaml --overlay /home/vagrant/maas-sandbox/cke/monitoring-pgt-overlay.yaml --overlay /home/vagrant/maas-sandbox/cke/logging-egf-overlay.yaml
 #
