45cd48c30856d29f6cc8cbf0155acf7af4a40e87
vpasias <52623837+vpasias@users.noreply.github.com>
2021-02-16 20:58:42 +0200

    Update 00300-deploy_basic_cke.sh

--- cke/00300-deploy_basic_cke.sh
+++ cke/00300-deploy_basic_cke.sh
@@ -3,7 +3,7 @@ wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays
 #wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/monitoring-pgt-overlay.yaml
 #wget https://raw.githubusercontent.com/charmed-kubernetes/bundle/master/overlays/logging-egf-overlay.yaml
 #
-juju deploy ./bundle.yaml --overlay ./calico-overlay.yaml --overlay ./monitoring-overlay.yaml --overlay ./logging-overlay.yaml
+juju deploy ./bundle.yaml --overlay ./calico-overlay.yaml --overlay ./monitoring-overlay.yaml --overlay ./logging-overlay.yaml --map-machines=existing
 # juju deploy bundle.yaml --overlay calico-overlay.yaml --overlay monitoring-overlay.yaml 
 #
 sudo snap install kubectl --classic
