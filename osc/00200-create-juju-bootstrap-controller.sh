ee60ac2fb777cfcd830729aa1ba2c0005b08508b
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-29 15:25:06 +0300

    Update 00200-create-juju-bootstrap-controller.sh

--- osc/00200-create-juju-bootstrap-controller.sh
+++ osc/00200-create-juju-bootstrap-controller.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-juju bootstrap --to node01.maas --config default-series=bionic maas-cloud maas-cloud-controller
+juju bootstrap --to node01.maas --config default-series=focal maas-cloud maas-cloud-controller
+#juju bootstrap --to node01.maas --config default-series=bionic maas-cloud maas-cloud-controller
 sleep 30
-juju add-model --config default-series=bionic openstack
+juju add-model --config default-series=focal openstack
+#juju add-model --config default-series=bionic openstack
